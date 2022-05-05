c925debfa70ea361333e0607c5bff6209deab634
Mouni9900 <98533290+Mouni9900@users.noreply.github.com>
2022-05-05 15:10:44 +0530

    Add files via upload

---
+++ April08.java
@@ -0,0 +1,16 @@
+class Test{
+	public void m( float f ) {
+		System.out.println("float");
+	}
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.m(10);
+	}
+	
+}
+	  
+			
+
+	
+
+
