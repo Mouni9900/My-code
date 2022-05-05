20ad80b6eebdec88689cefd9f35ac5d7292dbe5d
Mouni9900 <98533290+Mouni9900@users.noreply.github.com>
2022-05-05 15:21:43 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,31 @@
+public class Person {
+	private String firstname;
+	private String lastname;
+	private int birthmonth;
+	private int birthday;
+	private int birthyear;
+	private String ssn;
+	public String getfirstname() {
+		return firstname;
+		}
+	public String getlastname() {
+		return lastname;
+	}
+	public int getbirthmonth() {
+		return birthmonth;
+	}
+	public int getbirthday() {
+		return birthday;
+	}
+	public int getbirthyear() {
+		return birthyear;
+	}
+	public String getssn() {
+		return ssn;
+	}
+	public void setfirstname(String newname) {
+		firstname = newname;
+	}
+	
+
+}
